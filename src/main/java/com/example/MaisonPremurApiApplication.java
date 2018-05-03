6228913bec0883cd37fdcca98e64858fa8eefa71
KoutaGakuhari <voxphantom.c5@gmail.com>
2018-05-04 00:39:43 +0900

    CREATE NEW PROJECT

---
+++ src/main/java/com/example/MaisonPremurApiApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisonPremurApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisonPremurApiApplication.class, args);
+	}
+}
